a09429dc7074b60b3960ad1526938d144e98d09f
VladimirApter <vova.apter@gmail.com>
2024-07-05 16:47:32 +0500

    Add simple processing

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print('init')
+import telebot
+
+bot = telebot.TeleBot("7146497668:AAG5wBIrMEoho64W2F9MnSzYPyDPLrnqMto")
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Готов работать, отправь изображение или видео")
+
+@bot.message_handler(content_types=['photo'])
+def process_photo(message):
+    bot.reply_to(message, "Принял фото")
+
+@bot.message_handler(content_types=['video'])
+def process_video(message):
+    bot.reply_to(message, "Принял видео")
+
+bot.polling(none_stop=True)
