cc2c95087b6648a603de82a69ef2b3115808de3e
VladimirApter <vova.apter@gmail.com>
2024-07-23 15:41:14 +0500

    Fix even symbols drawing

--- ascii_artist/ascii_art_draw.py
+++ ascii_artist/ascii_art_draw.py
@@ -17,6 +17,6 @@ def create_img_font_drawer(original_img_size, font_size, bg_color=(0, 0, 0)):
 def draw_line(drawer, font, line, line_number, font_color=(255, 255, 255), true_color_mode=False, color_line=None):
     for x, symbol in enumerate(line):
         if true_color_mode:
-            drawer.text((x * font.size, line_number * font.size), symbol, font=font, fill=tuple(color_line[x]))
+            drawer.text((x * font.size + int(font.size / 5), line_number * font.size - int(font.size / 2.75)), symbol, font=font, fill=tuple(color_line[x]))
         else:
-            drawer.text((x * font.size, line_number * font.size), symbol, font=font, fill=font_color)
+            drawer.text((x * font.size + int(font.size / 5), line_number * font.size - int(font.size / 2.75)), symbol, font=font, fill=font_color)
